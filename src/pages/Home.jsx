8be32b0d92b80be6e47c5966c49f323ff7d7fd8b
Dallaz123 <154015587+Dallaz123@users.noreply.github.com>
2024-06-27 03:37:48 +0300

    Update Home&adding animations.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import main1 from "../assets/main1.png"
 import back1 from "../assets/back1.png"
 import main2 from "../assets/main2.png"
 import main3 from "../assets/main3.png"
-
 import main4 from "../assets/main4.png"
 import main5 from "../assets/main5.png"
 import main6 from "../assets/main6.png"
@@ -14,6 +13,8 @@ import { Link, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getMethod, postMethod } from "@/utils/ApiMethods"
 import { useToast } from "@/components/ui/use-toast"
+import 'aos/dist/aos.css'
+import Aos from "aos"
 
 export const HomeLoggedOut = () => {
     const { toast } = useToast()
@@ -48,19 +49,21 @@ export const HomeLoggedOut = () => {
         })
     }, [])
     const [email, setEmail] = useState("")
-
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, [])
     return (
         <div className="overflow-hidden">
             <div className="flex justify-center lg:justify-evenly font-cairo item-center mt-16 relative">
                 <img src={back1} alt="" className="absolute z-[-1] left-[-400px] w-[800px] opacity-35   top-[-400px] origin-center" />
                 <img src={back1} alt="" className="absolute z-[-1] right-[-400px] w-[800px] opacity-35   bottom-[-400px] origin-center" />
                 <div>
-                    <img src={main1} className="hidden lg:block w-[561px]" alt="" />
+                    <img src={main1} className="hidden lg:block w-[561px]" alt="" data-aos="fade-right" />
                 </div>
-                <div className="flex flex-col items-center mb-7 lg:mb-0  justify-around gap-8 lg:gap-0 lg:text-right">
-                    <p className="text-4xl lg:text-6xl font-extrabold text-[#2A3E34]">أكاديمية المنبــر</p>
-                    <div className="w-[95%] lg:w-[500px] text-center lg:text-right">
-                        <p className="text-[#3A5A40] text-xl font-bold">
+                <div className="flex flex-col items-center space-y-8 pt-36 lg:mb-0  gap-8 lg:gap-0 lg:text-right">
+                    <p className="text-4xl lg:text-6xl  font-extrabold text-[#2A3E34]" data-aos="fade-down">أكاديمية المنبـــــر</p>
+                    <div className="w-[95%] lg:w-[500px] text-center lg:text-right" data-aos="fade-up">
+                        <p dir="rtl" className="text-[#3A5A40] text-xl mb-10 mr-8 font-bold">
                             المنبر هي اكاديمية لتعليم العلوم الإسلامية الشرعية بــاستخــدام الذكــاء الاصطنــاعــي لتسهيــل العمليــة التعليمية،
                             وجعلها اكثر تفاعلية مع الطالب.
                         </p>
@@ -69,13 +72,13 @@ export const HomeLoggedOut = () => {
                                 <Button className="rounded-full bg-[#2a3e34] text-xl  px-7 hover:bg-[#395346]">تسجيل الدخول</Button>
                             </Link>
                             <Link to={"/register"}>
-                                <Button className="rounded-full bg-[#466746] text-xl px-7">إنشاء حساب</Button>
+                                <Button className="rounded-full bg-[#466746] text-xl px-7 ">إنشاء حساب</Button>
                             </Link>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="h-auto bg-[#2a3e34] mt-3 pb-4">
+            <div className="h-auto bg-[#2a3e34] mt-3 pb-4" data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000">
                 <p className="text-center text-white font-cairo text-3xl font-bold pt-8">ماذا تقدم أكاديمية منبـر</p>
                 <div className="flex text-center justify-evenly mt-8 flex-wrap h-auto">
                     <Each
@@ -95,9 +98,9 @@ export const HomeLoggedOut = () => {
                     <img src={back1} alt="" className="absolute z-[-1] left-[-620px] w-[1000px] opacity-35    origin-center" />
 
                     <div>
-                        <img src={main6} className="w-[561px] hidden lg:block" alt="" />
+                        <img src={main6} className="w-[561px] hidden lg:block" alt="" data-aos="flip-left" data-aos-offset="300" />
                     </div>
-                    <div className="flex flex-col justify-center gap-8 lg:items-end text-center lg:text-right">
+                    <div className="flex flex-col justify-center gap-8 lg:items-end text-center lg:text-right" data-aos = "fade-left" data-aos-offset="300">
                         <p className="text-4xl font-bold text-[#466746]">رسالة أكاديمية المنبر</p>
                         <div className="w-auto lg:w-[470px]">
                             <p className="text-[#2A3E34] text-xl">
@@ -116,8 +119,9 @@ export const HomeLoggedOut = () => {
                         </div>
                     </div>
                 </div>
-                <p className="text-[#2A3E34] font-extrabold text-4xl lg:text-5xl text-center  mt-6">تعلم على يد خيرة الشيوخ</p>
-                <div className="flex flex-wrap items-center justify-evenly mt-10 w-full  font-cairo ">
+                <div data-aos="fade-down" className="flex flex-wrap items-center justify-evenly mt-10 w-full  font-cairo ">
+                <p className="text-[#2A3E34] font-extrabold text-4xl lg:text-5xl text-center  mt-6" >تعلم على يد خيرة الشيوخ</p>
+                <div className="flex flex-wrap items-center justify-evenly mt-10 w-full  font-cairo " data-aos="zoom-in"> 
                     <Each
                         of={tutors}
                         render={(item, index) => (
@@ -130,19 +134,20 @@ export const HomeLoggedOut = () => {
                                     }
                                     alt=""
                                     className="w-[200px] h-[200px] rounded-full"
-                                />
+                                /><br/>
                                 <p className="text-[#2A3E34] text-xl font-bold">
                                     الشيخ {item.Lname} {item.Mname} {item.Fname}
                                 </p>
                                 <p className="text-[#3A5A40] text-lg">أستاذ وعالم في العلم الشرعي</p>
                                 <p className="text-[#2A3E34] text-lg font-bold">
-                                    يُدرِس {item.coursesToTeach[0] ? item.coursesToTeach[0].text : "العقيدة"}
+                                    يُدرِس: {item.coursesToTeach[0] ? item.coursesToTeach[0].text : "لا يدرس منهجاً"}
                                 </p>
                             </div>
                         )}
                     ></Each>
                 </div>
                 <Button
+                    data-aos="fade-left" data-aos-offset="50"
                     onClick={() => {
                         navigate("/tutors")
                     }}
@@ -153,6 +158,7 @@ export const HomeLoggedOut = () => {
                 <div
                     className="w-[90%]  lg:w-[60%] lg:min-w-[800px] flex lg:h-[450px] rounded-3xl flex-col lg:flex-row mt-24 mb-24"
                     style={{ boxShadow: "0px 0px 30px -15px rgba(0,0,0,0.64)" }}
+                    data-aos="fade-up" 
                 >
                     <div className="w-full lg:w-[70%] py-5 lg:py-0 h-full flex flex-col justify-center gap-5 text-center  items-center lg:text-right">
                         <div className="text-[#2a3e34]">
